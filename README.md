4af72c4314550814ab7a08b561c62e4d26074680
James Hogan <jhogan@jhogan.net>
2017-03-30 17:30:36 -0700

    complete v1.0

--- README.md
+++ README.md
@@ -3,3 +3,12 @@ Setup scripts to configure Hack Reactor pairing workstations.
 To run:  
 
 `curl -s https://raw.githubusercontent.com/jhogan4288/pairing-setup/master/pairing-setup.sh | bash`
+
+Current functionality:
+* Installs pomander
+* Installs [Dash](https://kapeli.com/dash), a great desktop developer reference app
+** You'll need to manually install most Dash docsets (can't be done from the command line, I tried to automate it).  `Dash` -> `Preferences` -> `Downloads`.  Get docsets for Javascript, Underscore, and whatever else you like.
+** I recommend setting a global hotkey under `Dash` -> `Preferences` -> `General`
+* Updates Sublime settings
+** `"tab_size": 2`
+** `"tanslate_tabs_to_spaces": true`
--- pairing-setup.sh
+++ pairing-setup.sh
@@ -4,22 +4,23 @@ cd $HOME
 
 # install macOS package manager in a location that allows us to install add'l packages without sudo access
 mkdir homebrew && curl -L https://github.com/Homebrew/brew/tarball/master | tar xz --strip 1 -C homebrew
-export BREW=$HOME/homebrew
-export BREWBIN=$BREW/bin
+export BREWDIR=$HOME/homebrew
+export BREWBINDIR=$BREWDIR/bin
+export BREWBIN=$BREWBINDIR/bin/brew
+
+# configure cask to install applications in our home directory
+export APPDIR=$HOME/Desktop/Applications
+export HOMEBREW_CASK_OPTS="--appdir=$APPDIR"
+mkdir -p $APPDIR
 
 # install and run Dash, great developer reference app
-$BREW cask install dash
-$BREWBIN/Dash.app
+$BREWBIN cask install dash
+open $APPDIR/Dash.app
 
 # install pomander
 curl -s https://raw.githubusercontent.com/reactorcore/pomander/master/bin/install | bash
 
-# update sublime settings
-$BREW install jq
-# $BREWBIN/jq '' $HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
-# "tab_size": 2
-# "tanslate_tabs_to_spaces": true
-
-
-
-
+# update sublime settings, using jq (command-line JSON manipulator)
+export $SUBLIMESETTINGS=$HOME/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/Preferences.sublime-settings
+$BREWBIN install jq
+$BREWBINDIR/jq 'tab_size = 2 | translate_tabs_to_spaces = true' $SUBLIMESETTINGS > $SUBLIMESETTINGS
